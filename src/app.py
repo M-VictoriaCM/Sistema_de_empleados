9338787f554b76b6e214f6d1c576ab925ead0441
M Victoria <mvittoria7@gmail.com>
2022-12-08 02:35:09 -0300

    Creacion de formulario para la creacion de empleados

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request,redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -19,13 +19,43 @@ def index():
     conn = mysql.connect()
     cursor= conn.cursor()
     
-    sql = "insert into empleados(nombre, correo, foto) values ('Juan', 'juan@gmail.com', 'fotosDeJuan.jpg');"
-    
+    sql = "SELECT * FROM empleados;"
     cursor.execute(sql)
+    
+    emplados = cursor. fetchall()
+    
     conn.commit()
     
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', 
+    empleados = empleados)
+    
+        
+@app.route('/create')
+def create():
+   return render_template('empleados/create.html')
+   
+   
+@app.route('/store', methods=["POST"])
+def store(): 
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.fiel['txtFoto']
 
+    sql = "INSERT INTO empleados(nombre, correo, foto) values (%s, %s, %s);"#s va porque son String
+    datos = (_nombre, _correo, _foto.fielname)
+
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql, datos) 
+    conn.commit()#aca se guarda en el base de datos
+    
+    return redirect('/')
+    
+
+
+   
+   
+   
 
 if __name__== '__main__':
     app.run(debug=True)
